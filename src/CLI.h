e72570d17f333f4f64c9eaa1aa251f25ba2001ce
Manuel Frischknecht <manuel.frischknecht@gmail.com>
2015-11-17 23:55:05 +0100

    Added default initalizers for static default arguments.

--- src/CLI.h
+++ src/CLI.h
@@ -39,6 +39,16 @@ two sampling points in the SRTM dataset (i.e. an arc second for the newest data)
 
 struct CommandLineArguments
 {
+	CommandLineArguments():
+		inputDirectory("."),
+		outputDirectory("."),
+
+		tileSize(0,0),
+		useCoarseSrtmResolution(false),
+		produceAsciiPly(false),
+		numThreads(0)
+	{}
+
 	std::string inputDirectory;
 	std::string outputDirectory;
 	std::array<WGS84::Point, 2> boundaryPoints;
